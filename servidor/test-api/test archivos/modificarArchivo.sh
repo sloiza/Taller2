040d68a73d31661083609f2493d06beedbdd7552
manueliglesias <m.iglesias91@gmail.com>
2015-11-23 01:18:51 -0300

    agregados test unitarios + test de api. #6

--- servidor/test-api/test archivos/modificarArchivo.sh
+++ servidor/test-api/test archivos/modificarArchivo.sh
@@ -1,5 +1,13 @@
 #!#bin/bash
 
+pathEntero=$1
+dir=${pathEntero%/*}/
+nombreYExt=${pathEntero##*/}
+ext=${nombreYExt##*.}
+nombre=${nombreYExt%.*}
 
-curl --data-binary "@pathDelArchivoARecuperar.json" -X POST "http://localhost:8080/archivos?modificar"
-curl --data-binary "@$1" -X POST "http://localhost:8080/archivos?modificar"
+if [ -z "$3" ]; then
+	curl -X PUT "http://localhost:8080/archivos?nombre=${nombre}&extension=${ext}&etiqueta={$2}&fecha_ulti_modi=26102015&usuario_ulti_modi=manu@gmail.com&propietario=manu@gmail.com&baja_logica=no&direccion=archivos/manu@gmail.com/"
+else
+	curl --data-binary "@$3" -X PUT "http://localhost:8080/archivos?nombre=${nombre}&extension=${ext}&etiqueta={$2}&fecha_ulti_modi=26102015&usuario_ulti_modi=manu@gmail.com&propietario=manu@gmail.com&baja_logica=no&direccion=archivos/manu@gmail.com/"
+fi
--- servidor/test-api/test archivos/subirArchivoNUEVO.sh
+++ servidor/test-api/test archivos/subirArchivoNUEVO.sh
@@ -1,3 +1,9 @@
 #!#bin/bash
 
-curl --data-binary "@$1" -X POST "http://localhost:8080/archivos?\{\"nombre\":\"archivoSubido\",\"extension\":\"test2\",\"etiqueta\":\"imagen\",\"fecha_ulti_modi\":\"26102015\",\"usuario_ulti_modi\":\"1234\",\"propietario\":\"manu@gmail.com\",\"baja_logica\":\"no\",\"direccion\":\"tmp/\"\}"
+pathEntero=$1
+dir=${pathEntero%/*}/
+nombreYExt=${pathEntero##*/}
+ext=${nombreYExt##*.}
+nombre=${nombreYExt%.*}
+
+curl --data-binary "@$1" -X POST "http://localhost:8080/archivos?nombre=${nombre}&extension=${ext}&etiqueta=imagen&fecha_ulti_modi=26102015&usuario_ulti_modi=manu@gmail.com&propietario=manu@gmail.com&baja_logica=no&direccion=archivos/manu@gmail.com/"
