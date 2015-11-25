ee1a1f8df807f4898fdac48f32a86a86e7e5ff72
sloiza <samiloiza@gmail.com>
2015-11-25 01:15:55 -0300

    changes dockerfile

--- servidor/install_dependencies.sh
+++ servidor/install_dependencies.sh
@@ -6,13 +6,10 @@ echo "install rocks"
 if [ ! -d "rocksdb-master/" ]; then
   	wget https://github.com/NickCis/rocksdb/archive/master.zip
 	unzip master.zip
+	rm master.zip
 	cd rocksdb-master
 	make static_lib
-	make shared_lib
-	sudo make install
-	cd ..
 	ls
-	rm master.zip
 fi
 cd /usr/src/gtest
 cmake .
@@ -24,5 +21,5 @@ ls
 cd /Taller2-master/servidor/
 mkdir build/
 cd build
-cmake ..
+cmake -DINCLUIR_TEST=ON ..
 make
