02b0071f48dd23139f4b0405a2d0c971d243ddeb
manueliglesias <m.iglesias91@gmail.com>
2015-11-25 15:37:46 -0300

    cheqear covergae.

--- servidor/coverage.sh
+++ servidor/coverage.sh
@@ -4,4 +4,6 @@ make
 make test
 #coveralls --exclude lib --exclude tests --gcov-options '\-lp'
 
+cd /Taller2-master/servidor/
+
 coveralls -i src -i test -E '.*\/mongoose\/mongoose\.[hc]' -E '.*CMakeFiles/feature_tests\..*' -E '.*CMakeCXXCompilerId\.cpp' -E '.*CMakeCCompilerId\.c' -E '.*\/test\/.*\.(h|hpp|cpp)$' -r ./
